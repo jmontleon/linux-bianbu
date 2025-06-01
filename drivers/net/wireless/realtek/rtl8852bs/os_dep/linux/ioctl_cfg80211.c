5cb704304d741eb7b4ca2985281ea4f8ba09089e
mara <mara@fail.pp.ua>
2025-06-01 03:07:18 +0300

    rtl8852bs duplicate code removed

--- drivers/net/wireless/realtek/rtl8852bs/os_dep/linux/ioctl_cfg80211.c
+++ drivers/net/wireless/realtek/rtl8852bs/os_dep/linux/ioctl_cfg80211.c
@@ -4645,9 +4645,6 @@ static int cfg80211_rtw_get_txpower(struct wiphy *wiphy,
 #if (defined(CONFIG_MLD_KERNEL_PATCH) || (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 2)))
 	unsigned int link_id,
 #endif
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0))
-	unsigned int link_id,
-#endif
 	int *dbm)
 {
 	RTW_INFO("%s\n", __func__);
