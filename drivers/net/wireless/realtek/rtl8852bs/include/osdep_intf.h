OctopusET/linux-k1-x1

wireless: rtl8852bs: add rtl8852bs sdio wifi driver
Change-Id: I747586fe062fa46dd58f105edc6d06b016d108bf

drivers/net/wireless/realtek/rtl8852bs/include/osdep_intf.h
@@ -1,0 +1,103 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2019 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ *****************************************************************************/
+#ifndef __OSDEP_INTF_H_
+#define __OSDEP_INTF_H_
+
+struct dvobj_priv *devobj_init(void);
+void devobj_deinit(struct dvobj_priv *pdvobj);
+u8 devobj_data_init(struct dvobj_priv *dvobj);
+void devobj_data_deinit(struct dvobj_priv *dvobj);
+u8 devobj_trx_resource_init(struct dvobj_priv *dvobj);
+void devobj_trx_resource_deinit(struct dvobj_priv *dvobj);
+
+u8 rtw_init_drv_sw(_adapter *padapter);
+u8 rtw_free_drv_sw(_adapter *padapter);
+u8 rtw_reset_drv_sw(_adapter *padapter);
+void rtw_drv_stop_prim_iface(_adapter *adapter);
+
+#if 0 /*#ifdef CONFIG_CORE_CMD_THREAD*/
+u32 rtw_start_drv_threads(_adapter *padapter);
+void rtw_stop_drv_threads(_adapter *padapter);
+#endif
+
+#if defined(CONFIG_WOWLAN) || defined(CONFIG_AP_WOWLAN)
+void rtw_cancel_dynamic_chk_timer(_adapter *padapter);
+#endif
+void rtw_cancel_all_timer(_adapter *padapter);
+
+
+#ifdef PLATFORM_LINUX
+int rtw_ioctl(struct net_device *dev, struct ifreq *rq, int cmd);
+
+int rtw_init_netdev_name(struct net_device *pnetdev, const char *ifname);
+struct net_device *rtw_init_netdev(_adapter *padapter);
+
+void rtw_os_ndev_free(_adapter *adapter);
+int rtw_os_ndev_init(_adapter *adapter, const char *name);
+void rtw_os_ndev_deinit(_adapter *adapter);
+void rtw_os_ndev_unregister(_adapter *adapter);
+void rtw_os_ndevs_unregister(struct dvobj_priv *dvobj);
+int rtw_os_ndevs_init(struct dvobj_priv *dvobj);
+void rtw_os_ndevs_deinit(struct dvobj_priv *dvobj);
+
+u16 rtw_os_recv_select_queue(u8 *msdu, enum rtw_rx_llc_hdl llc_hdl);
+
+int rtw_ndev_notifier_register(void);
+void rtw_ndev_notifier_unregister(void);
+void rtw_inetaddr_notifier_register(void);
+void rtw_inetaddr_notifier_unregister(void);
+
+#include "../os_dep/linux/rtw_proc.h"
+#include "../os_dep/linux/nlrtw.h"
+
+#ifdef CONFIG_IOCTL_CFG80211
+	#include "../os_dep/linux/ioctl_cfg80211.h"
+#endif /* CONFIG_IOCTL_CFG80211 */
+
+u8 rtw_rtnl_lock_needed(struct dvobj_priv *dvobj);
+void rtw_set_rtnl_lock_holder(struct dvobj_priv *dvobj, _thread_hdl_ thd_hdl);
+
+#endif /* PLATFORM_LINUX */
+
+
+#ifdef PLATFORM_FREEBSD
+extern int rtw_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data);
+#endif
+
+void rtw_ips_dev_unload(_adapter *padapter);
+
+#ifdef CONFIG_IPS
+int rtw_ips_pwr_up(_adapter *padapter);
+void rtw_ips_pwr_down(_adapter *padapter);
+#endif
+
+#ifdef CONFIG_CONCURRENT_MODE
+struct _io_ops;
+struct dvobj_priv;
+u8 rtw_drv_add_vir_ifaces(struct dvobj_priv *dvobj);
+void rtw_drv_stop_vir_ifaces(struct dvobj_priv *dvobj);
+void rtw_drv_free_vir_ifaces(struct dvobj_priv *dvobj);
+#endif
+
+void rtw_ndev_destructor(_nic_hdl ndev);
+#ifdef CONFIG_ARP_KEEP_ALIVE
+int rtw_gw_addr_query(_adapter *padapter);
+#endif
+
+int rtw_suspend_common(_adapter *padapter);
+int rtw_resume_common(_adapter *padapter);
+int rtw_suspend_free_assoc_resource(_adapter *padapter);
+
+#endif /* _OSDEP_INTF_H_ */

drivers/net/wireless/realtek/rtl8852bs/phl/phl_dfs.h
@@ -1,0 +1,42 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2019 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ *****************************************************************************/
+#ifndef _PHL_DFS_H_
+#define _PHL_DFS_H_
+
+#ifdef CONFIG_PHL_DFS
+bool phl_is_radar_detect_enabled(struct phl_info_t *phl_info, u8 band_idx);
+
+bool phl_is_under_cac(struct phl_info_t *phl_info, u8 band_idx);
+
+bool phl_is_cac_tx_paused(struct phl_info_t *phl_info, u8 band_idx);
+
+struct dfs_rd_ch_switch_ctx {
+	bool rd_enabled;
+	bool should_rd_en_on_new_ch;
+	bool under_cac;
+	bool cac_tx_paused;
+};
+
+void phl_dfs_rd_setting_before_ch_switch(struct phl_info_t *phl_info, u8 band_idx
+	, enum band_type band, u8 ch, enum channel_width bw, enum chan_offset offset, struct dfs_rd_ch_switch_ctx *ctx);
+
+void phl_dfs_rd_setting_after_ch_switch(struct phl_info_t *phl_info, u8 band_idx
+	, enum band_type band, u8 ch, enum channel_width bw, enum chan_offset offset, struct dfs_rd_ch_switch_ctx *ctx);
+
+enum rtw_phl_status
+phl_cmd_dfs_rd_ctl_hdl(struct phl_info_t *phl_info, u8 *param);
+#endif
+
+#endif /* _PHL_DFS_H_ */

jmontleon/linux-bianbu

drivers/net/wireless/realtek/rtl8852bs/include/osdep_intf.h
@@ -1,0 +1,100 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2019 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ *****************************************************************************/
+#ifndef __OSDEP_INTF_H_
+#define __OSDEP_INTF_H_
+
+struct dvobj_priv *devobj_init(void);
+void devobj_deinit(struct dvobj_priv *pdvobj);
+u8 devobj_data_init(struct dvobj_priv *dvobj);
+void devobj_data_deinit(struct dvobj_priv *dvobj);
+u8 devobj_trx_resource_init(struct dvobj_priv *dvobj);
+void devobj_trx_resource_deinit(struct dvobj_priv *dvobj);
+
+u8 rtw_init_drv_sw(_adapter *padapter);
+u8 rtw_free_drv_sw(_adapter *padapter);
+u8 rtw_reset_drv_sw(_adapter *padapter);
+void rtw_drv_stop_prim_iface(_adapter *adapter);
+
+u8 rtw_adapter_link_init(struct dvobj_priv *dvobj);
+void rtw_adapter_link_deinit(struct dvobj_priv *dvobj);
+int rtw_init_link_capab(struct dvobj_priv *dvobj);
+
+#if 0 /*#ifdef CONFIG_CORE_CMD_THREAD*/
+u32 rtw_start_drv_threads(_adapter *padapter);
+void rtw_stop_drv_threads(_adapter *padapter);
+#endif
+
+#if defined(CONFIG_WOWLAN) || defined(CONFIG_AP_WOWLAN)
+void rtw_cancel_dynamic_chk_timer(_adapter *padapter);
+#endif
+void rtw_cancel_all_timer(_adapter *padapter);
+
+
+#ifdef PLATFORM_LINUX
+int rtw_ioctl(struct net_device *dev, struct ifreq *rq, int cmd);
+
+int rtw_init_netdev_name(struct net_device *pnetdev, const char *ifname);
+struct net_device *rtw_init_netdev(_adapter *padapter);
+
+void rtw_os_ndev_free(_adapter *adapter);
+int rtw_os_ndev_init(_adapter *adapter, const char *name);
+void rtw_os_ndev_deinit(_adapter *adapter);
+void rtw_os_ndev_unregister(_adapter *adapter);
+void rtw_os_ndevs_unregister(struct dvobj_priv *dvobj);
+int rtw_os_ndevs_init(struct dvobj_priv *dvobj);
+void rtw_os_ndevs_deinit(struct dvobj_priv *dvobj);
+
+u16 rtw_os_recv_select_queue(u8 *msdu, enum rtw_rx_llc_hdl llc_hdl);
+
+int rtw_ndev_notifier_register(void);
+void rtw_ndev_notifier_unregister(void);
+void rtw_inetaddr_notifier_register(void);
+void rtw_inetaddr_notifier_unregister(void);
+
+#include "../os_dep/linux/rtw_proc.h"
+#include "../os_dep/linux/nlrtw.h"
+
+#ifdef CONFIG_IOCTL_CFG80211
+	#include "../os_dep/linux/ioctl_cfg80211.h"
+#endif /* CONFIG_IOCTL_CFG80211 */
+
+u8 rtw_rtnl_lock_needed(struct dvobj_priv *dvobj);
+void rtw_set_rtnl_lock_holder(struct dvobj_priv *dvobj, _thread_hdl_ thd_hdl);
+
+#endif /* PLATFORM_LINUX */
+
+
+#ifdef PLATFORM_FREEBSD
+extern int rtw_ioctl(struct ifnet *ifp, u_long cmd, caddr_t data);
+#endif
+
+#ifdef CONFIG_CONCURRENT_MODE
+struct _io_ops;
+struct dvobj_priv;
+u8 rtw_drv_add_vir_ifaces(struct dvobj_priv *dvobj);
+void rtw_drv_stop_vir_ifaces(struct dvobj_priv *dvobj);
+void rtw_drv_free_vir_ifaces(struct dvobj_priv *dvobj);
+#endif
+
+void rtw_ndev_destructor(_nic_hdl ndev);
+#ifdef CONFIG_ARP_KEEP_ALIVE_GW
+int rtw_gw_addr_query(_adapter *padapter);
+#endif
+
+int rtw_suspend_common(_adapter *padapter);
+int rtw_resume_common(_adapter *padapter);
+int rtw_suspend_free_assoc_resource(_adapter *padapter);
+
+#endif /* _OSDEP_INTF_H_ */

drivers/net/wireless/realtek/rtl8852bs/phl/phl_dfs.h
@@ -1,0 +1,42 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2019 Realtek Corporation.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ *****************************************************************************/
+#ifndef _PHL_DFS_H_
+#define _PHL_DFS_H_
+
+#ifdef CONFIG_PHL_DFS
+bool rtw_phl_is_radar_detect_enabled(struct rtw_phl_com_t *phl_com, u8 band_idx);
+
+bool rtw_phl_is_under_cac(struct rtw_phl_com_t *phl_com, u8 band_idx);
+
+bool phl_is_cac_tx_paused(struct phl_info_t *phl_info, u8 band_idx);
+
+struct dfs_rd_ch_switch_ctx {
+	bool rd_enabled;
+	bool should_rd_en_on_new_ch;
+	bool under_cac;
+	bool cac_tx_paused;
+};
+
+void phl_dfs_rd_setting_before_ch_switch(struct phl_info_t *phl_info, u8 band_idx
+	, enum band_type band, u8 ch, enum channel_width bw, enum chan_offset offset, struct dfs_rd_ch_switch_ctx *ctx);
+
+void phl_dfs_rd_setting_after_ch_switch(struct phl_info_t *phl_info, u8 band_idx
+	, enum band_type band, u8 ch, enum channel_width bw, enum chan_offset offset, struct dfs_rd_ch_switch_ctx *ctx);
+
+enum rtw_phl_status
+phl_cmd_dfs_rd_ctl_hdl(struct phl_info_t *phl_info, u8 *param);
+#endif
+
+#endif /* _PHL_DFS_H_ */